2bd5d01104e2f505da319432aa312d33717134c0
mohdarshad1 <mdarshad181@gmail.com>
2021-04-18 19:06:44 +0530

    [Mohd Arshad] : Added UC1

---
+++ SnakeAndLadder.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class SnakeAndLadder
+{
+//UC1 Initializing Starting position of game
+	static final int STARTING_POSITION = 0;
+	static final int ENDING_POSITION = 100;
+	public static void main(String args[])
+	{
+//Main method
+	SnakeAndLadder player = new SnakeAndLadder();
+	}
+}

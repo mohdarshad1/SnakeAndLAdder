a2e6e7aecf0368eed04b021b5509cd96e9baf785
mohdarshad1 <mdarshad181@gmail.com>
2021-04-18 19:13:57 +0530

    [Mohd Arshad] : Added UC2

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,9 +4,18 @@ class SnakeAndLadder
 //UC1 Initializing Starting position of game
 	static final int STARTING_POSITION = 0;
 	static final int ENDING_POSITION = 100;
+
+	public void RollingDice()
+	{
+		Random random = new Random();
+		int DICE = (random.nextInt(6)+1);
+		System.out.println("Rolling Dice :"+DICE);
+	}
+
 	public static void main(String args[])
 	{
 //Main method
-	SnakeAndLadder player = new SnakeAndLadder();
+	SnakeAndLadder Game1 = new SnakeAndLadder();
+	Game1.RollingDice();
 	}
 }

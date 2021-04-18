3b2e49cbe06ba8c27b4f6a9c4809fbafc747cd02
mohdarshad1 <mdarshad181@gmail.com>
2021-04-18 19:28:45 +0530

    [Mohd Arshad] : Added UC6

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -8,40 +8,42 @@ class SnakeAndLadder
 	{
 //UC2 Initializing Rolling Dice
 		Random random = new Random();
-		int currentPosition = STARTINGPOSITION ;
+		int currentposition = STARTINGPOSITION ;
+		String task ;
+		int total_dice_count = 0;
 // UC4 while loop for repet till player won
 		System.out.println("Game Played by the player");
 		System.out.println(" ");
-		while( currentPosition < ENDINGPOSITION )
+		while( currentposition < ENDINGPOSITION )
 		{
 			int DICE = (random.nextInt(6)+1);
+			total_dice_count++;
 //UC3 defining Snake, Ladder or no play
 			int action = random.nextInt(3);
-
-			//System.out.println("Rolling Dice : "+DICE);
-			//System.out.println("Action perform :"+action);
-			if(action == 1 && (currentPosition + DICE <= ENDINGPOSITION))
+			if(action == 1 && (currentposition + DICE <= ENDINGPOSITION))
 			{
-					currentPosition = currentPosition + DICE;
-			//		System.out.println("Player moves for Ladder");
+				currentposition = currentposition + DICE;
+				task = "Ladder";
+			//	System.out.println("Player moves for Ladder");
 			}
-			else if(action == 2 && (currentPosition - DICE >= STARTINGPOSITION))
+			else if(action == 2 && (currentposition - DICE >= STARTINGPOSITION))
 			{
-					currentPosition = currentPosition - DICE;
-			//		System.out.println("Player moves for Snake");
+
+				currentposition = currentposition - DICE;
+				task = "Snake";
+			//	System.out.println("Player moves for Snake");
 			}
 			else
 			{
-			//		 System.out.println("No Play");
+				task = "noPlay";
+			//	 System.out.println("No Play");
 			}
-//UC5 Player reaches at exact winning position 
-			System.out.println("Dice : "+DICE+" | Current Position : "+currentPosition);
-
+			System.out.println(" | Dice: "+DICE+" for "+task+" and Current Position : "+currentposition+"| ");
 		}
 		System.out.println(" ");
-
+//UC6 total times dice throws
+		System.out.println("Total Count of Dice Throws : "+total_dice_count);
 	}
-
 	public static void main(String args[])
 	{
 		SnakeAndLadder	Player = new SnakeAndLadder();	
